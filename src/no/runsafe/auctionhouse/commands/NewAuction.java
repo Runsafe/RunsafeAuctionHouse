9d2906f805096033b95c9e72fa61034b4ded8930
Morten Nilsen <morten@runsafe.no>
2013-07-19 02:44:10 +0200

    Upstream changes

--- src/no/runsafe/auctionhouse/commands/NewAuction.java
+++ src/no/runsafe/auctionhouse/commands/NewAuction.java
@@ -4,7 +4,7 @@ import no.runsafe.auctionhouse.AuctionWindowManager;
 import no.runsafe.framework.api.command.player.PlayerCommand;
 import no.runsafe.framework.minecraft.player.RunsafePlayer;
 
-import java.util.HashMap;
+import java.util.Map;
 
 public class NewAuction extends PlayerCommand
 {
@@ -15,7 +15,7 @@ public class NewAuction extends PlayerCommand
 	}
 
 	@Override
-	public String OnExecute(RunsafePlayer executor, HashMap<String, String> parameters)
+	public String OnExecute(RunsafePlayer executor, Map<String, String> parameters)
 	{
 		this.auctionWindowManager.openAuctionWindow(executor);
 		return null;
