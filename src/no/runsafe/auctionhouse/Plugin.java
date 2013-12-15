f93867f02a5470dce6f505f8042b5063f4acbd56
Morten Nilsen <morten@runsafe.no>
2013-12-15 12:53:32 +0100

    Declare features used

--- src/no/runsafe/auctionhouse/Plugin.java
+++ src/no/runsafe/auctionhouse/Plugin.java
@@ -3,12 +3,20 @@ package no.runsafe.auctionhouse;
 import no.runsafe.auctionhouse.commands.NewAuction;
 import no.runsafe.auctionhouse.database.AuctionsRepository;
 import no.runsafe.framework.RunsafeConfigurablePlugin;
+import no.runsafe.framework.features.Commands;
+import no.runsafe.framework.features.Database;
+import no.runsafe.framework.features.Events;
 
 public class Plugin extends RunsafeConfigurablePlugin
 {
 	@Override
 	protected void PluginSetup()
 	{
+		// Framework features
+		addComponent(Commands.class);
+		addComponent(Database.class);
+		addComponent(Events.class);
+
 		// Core
 		this.addComponent(AuctionWindowManager.class);
 		this.addComponent(AuctionHandler.class);
