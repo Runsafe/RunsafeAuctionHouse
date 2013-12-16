c6824d5122ef735c7b9331ea4abbff54c7c2bbc1
Morten Nilsen <morten@runsafe.no>
2013-12-16 16:31:27 +0100

    renamed upstream

--- src/no/runsafe/auctionhouse/Plugin.java
+++ src/no/runsafe/auctionhouse/Plugin.java
@@ -10,7 +10,7 @@ import no.runsafe.framework.features.Events;
 public class Plugin extends RunsafeConfigurablePlugin
 {
 	@Override
-	protected void PluginSetup()
+	protected void pluginSetup()
 	{
 		// Framework features
 		addComponent(Commands.class);
